de6d4d31592b6241aae10e30bf46a5a93522aed5
Stevie Howard <zahoward@gmail.com>
2016-07-07 22:40:26 +0000

    update flask app for deployment on C9.io

--- FlaskMySQLApp/app.py
+++ FlaskMySQLApp/app.py
@@ -1,29 +1,29 @@
 from flask import Flask, render_template, json, request
-from flask.ext.mysql import MySQL
-from werkzeug import generate_password_hash, check_password_hash
+from flaskext.mysql import MySQL
+from werkzeug.security import generate_password_hash, check_password_hash
+from os import getenv
 
 mysql = MySQL()
 app = Flask(__name__)
 
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'testuser'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'test623'
-app.config['MYSQL_DATABASE_DB'] = 'testdb'
+app.config['MYSQL_DATABASE_USER'] = 'csc370'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'project'
+app.config['MYSQL_DATABASE_DB'] = 'saiddit'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
 
-@app.route('/showHome')
+@app.route('/')
 def main():
     return render_template('index.html')
-
 
 @app.route('/showSignUp')
 def showSignUp():
     return render_template('signup.html')
 
 
-@app.route('/signUp', methods=['POST', 'GET'])
+@app.route('/signUp', methods=['POST','GET'])
 def signUp():
     try:
         _name = request.form['inputName']
@@ -38,22 +38,23 @@ def signUp():
             conn = mysql.connect()
             cursor = conn.cursor()
             _hashed_password = generate_password_hash(_password)
-            cursor.callproc('sp_createUser', (_name, _email, _hashed_password))
+            cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
             data = cursor.fetchall()
 
             if len(data) is 0:
                 conn.commit()
-                return json.dumps({'message': 'User created successfully !'})
+                return json.dumps({'message':'User created successfully !'})
             else:
-                return json.dumps({'error': str(data[0])})
+                return json.dumps({'error':str(data[0])})
         else:
-            return json.dumps({'html': '<span>Enter the required fields</span>'})
+            return json.dumps({'html':'<span>Enter the required fields</span>'})
 
     except Exception as e:
-        return json.dumps({'error': str(e)})
+        return json.dumps({'error':str(e)})
     finally:
         cursor.close()
         conn.close()
 
 if __name__ == "__main__":
-    app.run(port=5002)
+    # app.run()
+    app.run(host=getenv('IP', '0.0.0.0'), port=int(getenv('PORT', 8080)))
