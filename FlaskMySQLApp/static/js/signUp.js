1d012d0993792becf42edfe749beb27572f17f99
Stevie Howard <zahoward@gmail.com>
2016-07-08 16:40:18 +0000

    fix images

--- FlaskMySQLApp/static/js/signUp.js
+++ FlaskMySQLApp/static/js/signUp.js
@@ -1,6 +1,23 @@
 $(function(){
 	$('#btnSignUp').click(function(){
-		
+
+		$.ajax({
+			url: '/signUp',
+			data: $('form').serialize(),
+			type: 'POST',
+			success: function(response){
+				console.log(response);
+			},
+			error: function(error){
+				console.log(error);
+			}
+		});
+	});
+});
+
+$(function(){
+	$('#btnLogIn').click(function(){
+
 		$.ajax({
 			url: '/signUp',
 			data: $('form').serialize(),
