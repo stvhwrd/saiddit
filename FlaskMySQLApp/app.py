7b6e3d9b08ffca3558edae95c7d225865d5cc680
Stevie Howard <zahoward@gmail.com>
2016-07-12 10:08:34 -0700

    Remove email field; conform to pep8

--- FlaskMySQLApp/app.py
+++ FlaskMySQLApp/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, json, request
 from flaskext.mysql import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
 from os import getenv
-import sys #for stderr
+import sys  # for stderr
 
 mysql = MySQL()
 app = Flask(__name__)
@@ -19,47 +19,48 @@ mysql.init_app(app)
 def main():
     return render_template('index.html')
 
+
 @app.route('/showSignUp')
 def showSignUp():
     return render_template('signup.html')
+
 
 @app.route('/showLogIn')
 def showLogIn():
     return render_template('login.html')
 
-@app.route('/signUp', methods=['POST','GET'])
+
+@app.route('/signUp', methods=['POST', 'GET'])
 def signUp():
     try:
         username = request.form['inputName']
-        email = request.form['inputEmail']
         password = request.form['inputPassword']
         hashed_password = generate_password_hash(password, method="sha256", salt_length=8)
-        
+
         conn = mysql.connect()
-        cursor = conn.cursor()        
-       
+        cursor = conn.cursor()
+
         # validate the received values
         if username and password:
-            insert_stmt =  "INSERT INTO Accounts (username,password) VALUES (%s, %s)"
+            insert_stmt = "INSERT INTO Accounts (username,password) VALUES (%s, %s)"
             data = (username, hashed_password)
-            cursor.execute(insert_stmt,data)    
+            cursor.execute(insert_stmt, data)
             info = cursor.fetchone()
 
             if info is None:
                 conn.commit()
-                return json.dumps({'message':'User created successfully !'})
+                return json.dumps({'message': 'User created successfully !'})
             else:
-                return json.dumps({'error':str(data[0])})
+                return json.dumps({'error': str(data[0])})
         else:
-            return json.dumps({'html':'<span>Enter the required fields</span>'})
+            return json.dumps({'html': '<span>Enter the required fields</span>'})
 
     except Exception as e:
         sys.stderr.write(str(e))
-        return json.dumps({'error':str(e)})
+        return json.dumps({'error': str(e)})
     finally:
         cursor.close()
         conn.close()
 
 if __name__ == "__main__":
-    # app.run()
     app.run(host=getenv('IP', '0.0.0.0'), port=int(getenv('PORT', 8080)))
