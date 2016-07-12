20f099eca1fdbbf5bc2466ee23254be1c025de04
Stevie Howard <zahoward@gmail.com>
2016-07-12 10:40:02 -0700

    Remove duplicate insert statement

--- FlaskMySQLApp/app.py
+++ FlaskMySQLApp/app.py
@@ -28,37 +28,38 @@ def showSignUp():
 @app.route('/showLogIn')
 def showLogIn():
     return render_template('login.html')
-    
-    
-@app.route('/logIn', methods=['POST','GET'])
+
+
+@app.route('/logIn', methods=['POST', 'GET'])
 def logIn():
     try:
         username = request.form['inputName']
         password = request.form['inputPassword']
-        
+
         conn = mysql.connect()
         cursor = conn.cursor()
-        
+
         # If we got the username and password
         if username and password:
-            
             cursor.execute("SELECT password FROM Accounts WHERE username='"+username+"'")
-            password_hash = str(cursor.fetchone())[3:83] # becuase mysql adds 'u' to the front of the string and ',) to the end. Because we're converting from a tuple?
-            
+
+            # MySQL adds 'u' to the front of the string to denote unicode and ',) to the end.
+            password_hash = str(cursor.fetchone())[3:83]
+
             # check if password is correct else say that it was incorrect password or username
-            if check_password_hash(password_hash,password):
+            if check_password_hash(password_hash, password):
                 sys.stderr.write("correct username and password")
-                return json.dumps({'html':'<span>Enter the required fields</span>'})
+                return json.dumps({'html': '<span>Enter the required fields</span>'})
             else:
-               sys.stderr.write("Incorrect username or password")
-               return json.dumps({'html':'<span>Enter the required fields</span>'})
+                sys.stderr.write("Incorrect username or password")
+                return json.dumps({'html': '<span>Enter the required fields</span>'})
     except Exception as e:
         sys.stderr.write(str(e))
-        return json.dumps({'error':str(e)})
+        return json.dumps({'error': str(e)})
     finally:
         cursor.close()
-        conn.close()     
- 
+        conn.close()
+
 
 @app.route('/signUp', methods=['POST', 'GET'])
 def signUp():
@@ -73,9 +74,8 @@ def signUp():
 
         # validate the received values
         if username and password:
-            hashed_password = generate_password_hash(password,method="sha256", salt_length=8)
-            insert_stmt =  "INSERT INTO Accounts (username,password) VALUES (%s, %s)"
-            insert_stmt = "INSERT INTO Accounts (username,password) VALUES (%s, %s)"
+            hashed_password = generate_password_hash(password, method="sha256", salt_length=8)
+            insert_stmt = "INSERT INTO Accounts (username, password) VALUES (%s, %s)"
             data = (username, hashed_password)
             cursor.execute(insert_stmt, data)
             info = cursor.fetchone()
@@ -94,7 +94,7 @@ def signUp():
     finally:
         cursor.close()
         conn.close()
-        
+
 
 if __name__ == "__main__":
     app.run(host=getenv('IP', '0.0.0.0'), port=int(getenv('PORT', 8080)))
