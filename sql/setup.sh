235541951738b9914b44c1ff5d1b499d87c3d140
Stevie Howard <zahoward@gmail.com>
2016-07-15 17:03:10 -0700

    add compatibility with Mac OS X in setup script

--- sql/setup.sh
+++ sql/setup.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
-# execute a batch of commands in the mysql shell to create the saiddit database and tables
-mysql-ctl cli < ./db_schema.sql
+# Execute a batch of commands in the mysql shell to create
+# and populate the saiddit database and tables
+
+# Check which operating system we're on and execute accordingly
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+    # execute Mac instruction
+    mysql -u csc370 -p < ./db_schema.sql
+elif [[ "$(uname -s)" == 'Linux' ]]; then
+    # execute Linux (Cloud9) instruction
+    mysql-ctl cli < ./db_schema.sql
+fi
