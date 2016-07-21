e3b316cc94298b9e134fe6bfcb59a4f1d7208b04
Sterling Laird <lairdsterling@gmail.com>
2016-07-21 19:43:25 +0000

    forgot to commit this file with last commit

--- FlaskMySQLApp/static/js/index.js
+++ FlaskMySQLApp/static/js/index.js
@@ -3,6 +3,8 @@ function getPosts(){
     $.ajax({
 		url: '/getPosts',
 		type: 'GET',
+		
+		//If the front page doesnt load right, try changing the query to this? SELECT * FROM Posts JOIN (SELECT * FROM Subsaiddits WHERE front_page) AS sub ON subsaiddit=sub.title ORDER BY (upvotes-downvotes) DESC LIMIT 12
 		data: {'query':'SELECT Posts.post_id, Posts.publish_time, Posts.edit_time, Posts.title, Posts.url, Posts.body, Posts.upvotes, Posts.downvotes, Posts.subsaiddit, Posts.author_key FROM Posts JOIN Subsaiddits ON subsaiddit=Subsaiddits.title ORDER BY (upvotes-downvotes) DESC LIMIT 12'},
 		async: false,
 		success: function(response){
@@ -16,6 +18,3 @@ function getPosts(){
 	});
 	return val;
 }
-
-//data: {'query':'SELECT post_id, publish_time, edit_time, title, url, body, upvotes, downvotes, subsaiddit, author_key FROM Posts JOIN (SELECT * FROM Subsaiddits WHERE front_page) AS sub ON subsaiddit=sub.title ORDER BY (upvotes-downvotes) DESC LIMIT 12'},
-		
